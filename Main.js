48909ec7fa2e0828389f2d3210e0d3c8bc1de924
Lu.cks <112194186+Lucks-git@users.noreply.github.com>
2026-02-21 08:08:48 -0300

    a

--- Main.js
+++ Main.js
@@ -1,10 +1,10 @@
 window.addEventListener("load", function () {
     const loader = document.getElementById("loader");
     const button = document.querySelector(".MInfo");
-    
+
     loader.classList.add("hidden");
 
-    button.addEventListener('click', () => {
+    button.addEventListener("click", () => {
 
          button.classList.toggle("active")
          console.log("fuck you")
