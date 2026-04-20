61126fe429f2d4f4897802cf7bc3acb18b8d13f8
Lu.cks <112194186+Lucks-git@users.noreply.github.com>
2026-04-20 10:42:11 -0300

    PLUH

--- Main.js
+++ Main.js
@@ -19,7 +19,7 @@ window.addEventListener("load", function () {
     }
     instance.onclick = function (){
         instance.classList.toggle("active");
-        window.locarion.href = "https://www.roblox.com/users/1222144987/profile";
+        window.locarion.href = "https://www.roblox.com/users/1222144987";
     }
 
     loader.classList.add("hidden");
